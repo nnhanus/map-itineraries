00132383753154077b09eaaa38cf294062524a25
unknown <noemie.hanus@inria.fr>
2024-02-12 17:12:51 +0100

    swipe map to get distance + info when close + request around place

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 var map = L.map('map', {dragging: false}).setView([52.19226,0.15216], 16);
-
+var polyline;
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     // minZoom: 10,
@@ -28,66 +28,111 @@ var routing = L.Routing.control({
     routeLine: function(route, options) {
         line = L.Routing.line(route, options);
         
-        // line.eachLayer(function(l) {
-        //     l.on("mousedown", function(e){
-        //         // console.log("mouse down");
-        //         isPointerDown = true;
-        //         map.dragging.disable();
-        //     })
-        // });
+        
         return line;
     }
 }).addTo(map);
 
 
+function toPixels(latlng){
+    return map.latLngToContainerPoint(latlng);
+}
+
+function buildPosPixels(latlng){
+    allPosPixels.push(toPixels(latlng));
+}
 
 var allPos;
+var allPosPixels  = new Array();
 var distance;
 var time;
 var points = new Array();
 
 routing.on("routesfound", function (e){
     allPos = e.routes[0].coordinates;
+    // console.log(allPos.length);
     distance = e.routes[0].summary.totalDistance;
     time = e.routes[0].summary.totalTime;
-    
-    
+    // allPos.forEach(buildPosPixels);
+    polyline = L.polyline(allPos, {color: 'red'}).addTo(map);
+
     // map.removeLayer(line);
     
     console.log("routesfound; dist = " + distance + " m; time = " + toMinutes(time));
 })
 
+var popupdist = L.popup({maxWidth: 200}); 
+// var opl = new L.OverPassLayer({
+//     query: "area['name'='Orsay']; node(area)['amenity'='restaurant'];out;",
+//     // onSuccess: function(data) {
 
-var opl = new L.OverPassLayer({
-    query: "area['name'='Orsay']; node(area)['amenity'='restaurant'];out;",
-    // onSuccess: function(data) {
 
-
-    //     for(i=0;i<data.elements.length;i++) {
-    //       e = data.elements[i];
+//     //     for(i=0;i<data.elements.length;i++) {
+//     //       e = data.elements[i];
           
        
         
-    //       var pos = new L.LatLng(e.lat, e.lon);
-    //       var color = 'green';
-    //     //   L.marker([closest.lat, closest.lng], ).addTo(map);
-    //       L.marker(pos, {icon: greenIcon}).addTo(map);
-    //     //   L.circle(pos, 5, {
-    //     //     color: color,
-    //     //     fillColor: '#fa3',
-    //     //     fillOpacity: 1,
-    //     //   }).addTo(map);
+//     //       var pos = new L.LatLng(e.lat, e.lon);
+//     //       var color = 'green';
+//     //     //   L.marker([closest.lat, closest.lng], ).addTo(map);
+//     //       L.marker(pos, {icon: greenIcon}).addTo(map);
+//     //     //   L.circle(pos, 5, {
+//     //     //     color: color,
+//     //     //     fillColor: '#fa3',
+//     //     //     fillOpacity: 1,
+//     //     //   }).addTo(map);
          
-    //     }
-    //   },
-    });
+//     //     }
+//     //   },
+//     });
 
-map.addLayer(opl);
+// map.addLayer(opl);
 
+var previousLatLng;
 
+function getDistanceInCM(latlng, point){
+    var closest = L.GeometryUtil.closest(map, allPos, latlng);
+    var closestPixel = toPixels(closest);
+    return ((point.distanceTo(closestPixel)*2.54/(269/window.devicePixelRatio)));
+}
+function distancePixelPoints(latlng, point){
+    var dist = getDistanceInCM(latlng, point);
+    var closest = L.GeometryUtil.closest(map, allPos, latlng);
+    
+    if (dist < 0.5 ){
+        // if (point.distanceTo(previousLatLng) > 3){
+        // console.log( point.distanceTo(previousLatLng) );
+        
+        previousLatLng = point;
+        isPointOnLine(closest, allPos, 5)
+        onMap = true;
+        points.push(closest);
+        var dist = 0;
+        for (var i = 0; i < points.length - 1; i++){
+            dist += points[i].distanceTo(points[i+1]);
+        }
+        var percent = dist*100/distance;
+        popupdist
+            .setLatLng(latlng)
+            .setContent("Distance from start of the route: " + (dist/1000).toFixed(2) + " km (" + percent.toFixed(1) + "%). Time on arrival : " + inHours(percent*time/100))
+            .openOn(map);
+        // }
+    } else {
+        popupdist
+            .setLatLng(latlng)
+            // .setContent("point : " + pointPixel + ", closest : " + closestPixel + ", distance : " + pointPixel.distanceTo(closestPixel) + ", cm : " + ((pointPixel.distanceTo(closestPixel)*2.54/269)))
+            // .setContent("closestlayerpoint : " + polyline.closestLayerPoint(point) + ", closest latlng : " + closest + ", closest pixels : " + closestPixel)
+            // .setContent("cm : " + ((point.distanceTo(closestPixel)*2.54/(269* window.devicePixelRatio))) + "cm w/ : " + ((point.distanceTo(closestPixel)*2.54/(269))) +  "cm / : " + ((point.distanceTo(closestPixel)*2.54/(269/window.devicePixelRatio))))
+            .setContent("distance (cm): " + dist)
+            .openOn(map);
 
+    }
 
-
+}
+ 
+function getResolution() {
+    alert("Your screen resolution is: " + (screen.width* window.devicePixelRatio) + "x" + (screen.height* window.devicePixelRatio));
+}
 
 
 var greenIcon = new L.Icon({
@@ -223,6 +268,7 @@ onpointerdown = (event) => {
     // window.alert("pointer");
     event.preventDefault();
     isPointerDown = true;
+    
     // var touch = event.touches[0];
     // var point = L.point(touch.screenX, touch.screenY);
     // var latlng = map.containerPointToLatLng(point);
@@ -230,10 +276,11 @@ onpointerdown = (event) => {
     
     var point = L.point(event.clientX, event.clientY);
     var latlng = map.containerPointToLatLng(point);
-    if(isPointOnLine(latlng, allPos)){
-        onMap = true;
-        // map.dragging.disable();
-    }
+    previousLatLng = point;
+    // if(isPointOnLine(latlng, allPos)){
+    //     onMap = true;
+    //     // map.dragging.disable();
+    // }
 };
 
 onpointermove = (event) => {
@@ -243,27 +290,29 @@ onpointermove = (event) => {
         // var point = L.point(touch.screenX, touch.screenY);
         var point = L.point(event.clientX, event.clientY);
         var latlng = map.containerPointToLatLng(point);
+        // console.log("point: " + point + "containerpoint : " + toPixels(latlng));
+        distancePixelPoints(latlng, point);
         
-        if(isPointOnLine(latlng, allPos, 2)){
-            // window.alert(latlng);
-            onMap = true;
-            points.push(latlng);
-            var dist = 0;
-            for (var i = 0; i < points.length - 1; i++){
-                dist += points[i].distanceTo(points[i+1]);
-            }
-            var percent = dist*100/distance;
+        // if(isPointOnLine(latlng, allPos, 2)){
+        //     // window.alert(latlng);
+        //     onMap = true;
+        //     points.push(latlng);
+        //     var dist = 0;
+        //     for (var i = 0; i < points.length - 1; i++){
+        //         dist += points[i].distanceTo(points[i+1]);
+        //     }
+        //     var percent = dist*100/distance;
             
-            // var inhour = hours(percent*time/100);
-            // var inminutes = minutes(percent*time/100);
-            // console.log(hour + ":" + min);
-            popupdrag
-            .setLatLng(latlng)
-            .setContent("Distance from start of the route: " + (dist/1000).toFixed(2) + " km (" + percent.toFixed(1) + "%). Time on arrival : " + inHours(percent*time/100))
-            .openOn(map);
-            // console.log(e);
-            points.length = 0;
-        }
+        //     // var inhour = hours(percent*time/100);
+        //     // var inminutes = minutes(percent*time/100);
+        //     // console.log(hour + ":" + min);
+        //     popupdrag
+        //     .setLatLng(latlng)
+        //     .setContent("Distance from start of the route: " + (dist/1000).toFixed(2) + " km (" + percent.toFixed(1) + "%). Time on arrival : " + inHours(percent*time/100))
+        //     .openOn(map);
+        //     // console.log(e);
+        //     points.length = 0;
+        // }
     }
 };
 
@@ -275,25 +324,47 @@ onpointerup = (event) => {
         var point = L.point(event.clientX, event.clientY);
         var latlng = map.containerPointToLatLng(point);
         // map.closePopup();
-        if(isPointOnLine(latlng, allPos, 5) && onMap){
-            // window.alert("true");
-            var marker = new L.marker(latlng, {draggable: 'false'}).addTo(map);
-            marker.on("dragstart", function (e){
-                console.log("dragstart");
-            });
+        // if(isPointOnLine(latlng, allPos, 5) && onMap){
+        //     // window.alert("true");
+        //     var marker = new L.marker(latlng, {draggable: 'false'}).addTo(map);
+        //     marker.on("dragstart", function (e){
+        //         console.log("dragstart");
+        //     });
             
-            points.push(latlng);
+        //     points.push(latlng);
+        //     var dist = 0;
+        //     for (var i = 0; i < points.length - 1; i++){
+        //         dist += points[i].distanceTo(points[i+1]);
+        //     }
+        //     var percent = dist*100/distance;
+            
+        //     marker.bindPopup("Distance from start of the route: " + (dist/1000).toFixed(2) + " km (" + percent.toFixed(1) + "%). Time on arrival : " + inHours(percent*time/100), {maxWidth : 200}).openPopup();
+            
+        //     // console.log(e);
+        //     points.length = 0;
+        
+        if(getDistanceInCM(latlng, point) < 0.5){
+            var closest = L.GeometryUtil.closest(map, allPos, latlng);
+            var marker = new L.marker(closest, {draggable: 'false'}).addTo(map);
+            isPointOnLine(closest, allPos, 5)
+            points.push(closest);
             var dist = 0;
             for (var i = 0; i < points.length - 1; i++){
                 dist += points[i].distanceTo(points[i+1]);
             }
             var percent = dist*100/distance;
-            
+
             marker.bindPopup("Distance from start of the route: " + (dist/1000).toFixed(2) + " km (" + percent.toFixed(1) + "%). Time on arrival : " + inHours(percent*time/100), {maxWidth : 200}).openPopup();
-            
-            // console.log(e);
-            points.length = 0;
+
+            var opl = new L.OverPassLayer({
+                minZoom: 9,
+                query: `node(around: 5000.0, ${latlng.lat}, ${latlng.lng})['amenity'='restaurant'];out;`,
+            });
+            map.addLayer(opl);
+
         }
+            
+        // }
         onMap = false;
         isPointerDown = false;
         // map.dragging.enable();
