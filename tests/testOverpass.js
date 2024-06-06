11be3925332ef7a2b7b9a61366426bb6c5726e0a
Emmanuel Pietriga <emmanuel.pietriga@inria.fr>
2024-06-06 16:01:09 +0200

    simple JS tests for both overpass and openrouteservice local servers

---
+++ tests/testOverpass.js
@@ -0,0 +1,34 @@
+console.log("querying...");
+fetch(
+    "https://mapitin.lisn.upsaclay.fr:9000/api/interpreter",
+    {
+        method: "POST",
+        // The body contains the query
+        // to understand the query language see "The Programmatic Query Language" on
+        // https://wiki.openstreetmap.org/wiki/Overpass_API#The_Programmatic_Query_Language_(OverpassQL)
+        body: "data="+ encodeURIComponent(`
+            [bbox:30.618338,-96.323712,30.591028,-96.330826]
+            [out:json]
+            [timeout:90]
+            ;
+            (
+                way
+                    (
+                         30.626917110746,
+                         -96.348809105664,
+                         30.634468750236,
+                         -96.339893442898
+                     );
+            );
+            out geom;
+        `)
+    },
+).then(
+    function(data){
+        console.log("response");
+        console.log(data.json().then(function (json) { console.log(json); console.log("done"); }));
+
+    }
+)
+
+//
